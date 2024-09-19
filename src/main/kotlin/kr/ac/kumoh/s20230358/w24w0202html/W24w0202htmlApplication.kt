eee57d3fab0c5c36349f2b27ae87015283bc111f
hyobin77 <gyqls0509@kumoh.ac.kr>
2024-09-19 10:56:52 +0900

    HTML 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20230358/w24w0202html/W24w0202htmlApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230358.w24w0202html
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w0202htmlApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w0202htmlApplication>(*args)
+}
